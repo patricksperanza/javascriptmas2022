28afc1778c02e124a347304862628c2fa2710a98
Patrick Speranza <patrick.m.speranza@gmail.com>
2022-12-07 19:52:38 -0600

    added day 8 challenge

---
+++ day8.js
@@ -0,0 +1,60 @@
+function validTime(str) {
+  let nums = str.split(":")
+  if (
+    Number(nums[0]) >= 0 &&
+    Number(nums[0]) < 24 &&
+    Number(nums[1]) >= 0 &&
+    Number(nums[1]) < 60
+  ) {
+    return true
+  } else {
+    return false
+  }
+}
+
+/**
+ * Test Suite
+ */
+describe("validTime()", () => {
+  it("returns true for valid time", () => {
+    // arrange
+    const str = "13:58"
+
+    // act
+    const result = validTime(str)
+
+    // log
+    console.log("result 1: ", result)
+
+    // assert
+    expect(result).toBe(true)
+  })
+
+  it("returns false when invalid hours", () => {
+    // arrange
+    const str = "25:51"
+
+    // act
+    const result = validTime(str)
+
+    // log
+    console.log("result 1: ", result)
+
+    // assert
+    expect(result).toBe(false)
+  })
+
+  it("returns false when invalid minutes", () => {
+    // arrange
+    const str = "02:76"
+
+    // act
+    const result = validTime(str)
+
+    // log
+    console.log("result 1: ", result)
+
+    // assert
+    expect(result).toBe(false)
+  })
+})
