1b1734f5356675f53682407b32df25185e8ca67e
Patrick Speranza <patrick.m.speranza@gmail.com>
2022-12-17 21:24:45 -0600

    added day18

---
+++ day18.js
@@ -0,0 +1,19 @@
+
+function candies(children, candy) {
+   return children * Math.floor(candy / children)
+}
+
+
+
+/**
+* Test Suite 
+*/
+describe('candies()', () => {
+
+Find
+No Results
+
+CONSOLE
+›result: ,9
+/index.html
+
